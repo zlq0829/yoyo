44e3bcfe111a8c92f34e7bc8b1652dc5de20548f
zhangyunbin <1135534394@qq.com>
2022-03-31 13:54:57 +0800

    修改版本更新逻辑

--- main.js
+++ main.js
@@ -13,34 +13,40 @@ function isDev(env) {
   return reg;
 }
 
+// 版本更新
 function checkUpdate() {
   autoUpdater.setFeedURL('oss://yoyolivewebpack-test/yoyo/win32-x64/')
+
+  // 检测更新
+  autoUpdater.checkForUpdates()
+
+  //监听'error'事件
+  autoUpdater.on('error', (err) => {
+    console.log(err)
+  })
+
+  //监听'update-available'事件，发现有新版本时触发
+  autoUpdater.on('update-available', () => {
+    console.log('found new version')
+  })
+
+
+  //监听'update-downloaded'事件，新版本下载完成时触发
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: '应用更新',
+      message: '发现新版本，是否更新？',
+      buttons: ['是', '否']
+    }).then((buttonIndex) => {
+      if(buttonIndex.response == 0) {  //选择是，则退出程序，安装新版本
+        autoUpdater.quitAndInstall()
+        app.quit()
+      }
+    })
+  })
 }
 
-//监听'error'事件
-autoUpdater.on('error', (err) => {
-  console.log(err)
-})
-
-//监听'update-available'事件，发现有新版本时触发
-autoUpdater.on('update-available', () => {
-  console.log('found new version')
-})
-
-//监听'update-downloaded'事件，新版本下载完成时触发
-autoUpdater.on('update-downloaded', () => {
-  dialog.showMessageBox({
-    type: 'info',
-    title: '应用更新',
-    message: '发现新版本，是否更新？',
-    buttons: ['是', '否']
-  }).then((buttonIndex) => {
-    if(buttonIndex.response == 0) {  //选择是，则退出程序，安装新版本
-      autoUpdater.quitAndInstall()
-      app.quit()
-    }
-  })
-})
 
 if (!isDev(process.env.NODE_ENV)) {
   cwd = path.join(__dirname, '..', 'server');
